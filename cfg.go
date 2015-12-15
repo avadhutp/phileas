1cbe734c2d13202a37e6ac423edf169d347bcf7c
avadhutp <avadhutp@yelp.com>
2015-12-15 20:37:27 +0530

    Cleaner dump code.

--- cfg.go
+++ cfg.go
@@ -10,24 +10,31 @@ type common struct {
 	Port int `ini:"port"`
 }
 
+type instagram struct {
+	Secret string `ini:"secret"`
+	Token  string `ini:"access_token"`
+}
+
 // Cfg Config map for phileas.ini
 type Cfg struct {
-	Common common `ini:"common"`
+	Common    common    `ini:"common"`
+	Instagram instagram `ini:"instagram"`
 }
 
 func (cfg *Cfg) dump() {
-	dumpSection(cfg.Common)
+	dumpSection(cfg.Common, "Common")
+	dumpSection(cfg.Instagram, "Instagram")
 }
 
-func dumpSection(sect interface{}) {
+func dumpSection(sect interface{}, sectName string) {
 	m := structs.Map(sect)
 
 	for name, val := range m {
 		switch v := val.(type) {
 		case int:
-			logger.Info(fmt.Sprintf("%s = %d", name, v))
+			logger.Info(fmt.Sprintf("%s.%s = %d", sectName, name, v))
 		default:
-			logger.Info(fmt.Sprintf("%s = %s", name, v))
+			logger.Info(fmt.Sprintf("%s.%s = %s", sectName, name, v))
 		}
 	}
 }
