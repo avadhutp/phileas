f60703d9d527249b2a80e625db24f8284ca3d44a
avadhutp <avadhutp@yelp.com>
2015-12-21 18:20:32 +0530

    Struct for engine.

--- lib/service.go
+++ lib/service.go
@@ -8,20 +8,29 @@ import (
 
 // NewService Provides the gonic gin service
 func NewService(cfg *Cfg) *gin.Engine {
+	eng := &PhileasEngine{cfg}
+
 	s := gin.New()
 	s.Use(gin.Logger())
 	s.LoadHTMLGlob("templates/*")
 
-	s.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	s.GET("/top", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "mapper.tmpl", gin.H{
-			"title": "Top destinations",
-			"key":   cfg.Common.GoogleMapsKey,
-		})
-	})
+	s.GET("/ping", eng.ping)
+	s.GET("/top", eng.mapper)
 
 	return s
+}
+
+type PhileasEngine struct {
+	cfg *Cfg
+}
+
+func (pe *PhileasEngine) ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func (pe *PhileasEngine) mapper(c *gin.Context) {
+	c.HTML(http.StatusOK, "mapper.tmpl", gin.H{
+		"title": "Top destinations",
+		"key":   pe.cfg.Common.GoogleMapsKey,
+	})
 }
