ef2457f732c2ac24dc8f9dcb37208d087852b6f1
avadhutp <avadhutp@yelp.com>
2016-01-05 02:29:15 +0530

    Added script-hack for codecov.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Runs go tests in all subpackages and pushes details out to codecov via travis-ci
+
+set -e
+echo "" > coverage.txt
+
+for d in $(find ./* -maxdepth 10 -type d); do
+    if ls $d/*.go &> /dev/null; then
+        go test -coverprofile=profile.out -covermode=atomic $d
+        if [ -f profile.out ]; then
+            cat profile.out >> coverage.txt
+            rm profile.out
+        fi
+    fi
+done
